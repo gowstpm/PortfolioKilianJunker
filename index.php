b1418f52deec6af15ad10493d0b537f921bcc3da
gowstpm <69219356+gowstpm@users.noreply.github.com>
2024-10-24 14:27:51 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PortFolio Kilian Junker</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header id="header">
+    <div class="d-flex flex-column">
+
+      <div class="profile">
+        <img src="images/1" alt="Profile Picture" class="img-fluid rounded-circle">
+        <h1 class="text-light text-center"><a href="index.html">Kilian Junker</a></h1>
+        <div class="social-links mt-3 text-center">
+          <a href="https://www.linkedin.com/in/kilian-junker" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+      </div>
+
+      <nav id="navbar" class="nav-menu">
+        <ul>
+          <li><a href="#hero" class="link"><i class="bx bx-home"></i> <span>Accueil</span></a></li>
+          <li><a href="#about" class="link"><i class="bx bx-user"></i> <span>À propos</span></a></li>
+          <li><a href="#formation" class="link"><i class="bi bi-mortarboard"></i> <span>Formation</span></a></li>
+          <li><a href="#skills" class="link"><i class="bi bi-folder"></i> <span>Compétences</span></a></li>
+          <li><a href="#resume" class="link active"><i class="bx bx-file"></i> <span>Parcours</span></a></li>
+          <li><a href="#portfolio" class="link"><i class="bx bx-book"></i> <span>Expériences</span></a></li>
+          <li><a href="#services" class="link"><i class="bi bi-newspaper"></i> <span>Veille</span></a></li>
+          <li><a href="#contact" class="link"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <!-- Sections -->
+  <section id="hero" class="home">
+    <div class="title">
+      <h1>Kilian Junker <br>Alternant BTS SIO</h1>
+    </div>
+  </section>
+  
+  <section id="about">
+    <div class="content">
+      <h2>À propos</h2>
+      <p>Information sur moi.</p>
+    </div>
+  </section>
+
+  <section id="formation">
+    <div class="content">
+      <h2>Formation</h2>
+      <p>Détails sur mes études.</p>
+    </div>
+  </section>
+
+  <section id="skills">
+    <div class="content">
+      <h2>Compétences</h2>
+      <p>Liste de mes compétences.</p>
+    </div>
+  </section>
+
+  <section id="resume">
+    <div class="content">
+      <h2>Parcours</h2>
+      <p>Mon parcours professionnel.</p>
+    </div>
+  </section>
+
+  <section id="portfolio">
+    <div class="content">
+      <h2>Expériences</h2>
+      <p>Mes expériences professionnelles.</p>
+    </div>
+  </section>
+
+  <section id="services">
+    <div class="content">
+      <h2>Veille</h2>
+      <p>Ma veille technologique.</p>
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="content">
+      <h2>Contact</h2>
+      <p>Contactez-moi pour plus d'informations.</p>
+    </div>
+  </section>
+
+</body>
+</html>
