e70f99b75df99f56efefb6c350708e0b15aeda8b
gowstpm <69219356+gowstpm@users.noreply.github.com>
2024-10-24 17:00:41 +0200

    2 commit

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
     <div class="d-flex flex-column">
 
       <div class="profile">
-        <img src="images/1" alt="Profile Picture" class="img-fluid rounded-circle">
+        <img src="images/image1.png" alt="Profile Picture">
         <h1 class="text-light text-center"><a href="index.html">Kilian Junker</a></h1>
-        <div class="social-links mt-3 text-center">
-          <a href="https://www.linkedin.com/in/kilian-junker" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        <div>
+          <a href="https://www.linkedin.com/in/kilian-junker" class="linkedin"></a>
         </div>
       </div>
 
@@ -34,44 +34,146 @@
   </header>
 
   <!-- Sections -->
-  <section id="hero" class="home">
+  <div class="home">
     <div class="title">
       <h1>Kilian Junker <br>Alternant BTS SIO</h1>
     </div>
-  </section>
-  
+  </div>
+
   <section id="about">
     <div class="content">
-      <h2>À propos</h2>
-      <p>Information sur moi.</p>
+      <div class="about-content">
+        <img src="images/image1.png" alt="Kilian Junker" class="about-image">
+        <div class="about-text">
+          <h2>À propos</h2>
+          <div class="ligne"></div>
+          <p>Je suis Kilian Junker, un développeur web passionné et un professionnel du support informatique. J'ai une solide expérience dans la création de sites web en utilisant les langages de code tels que HTML, CSS et JavaScript. J'ai développé plusieurs sites web, en mettant l'accent sur l'esthétique, la convivialité et les fonctionnalités interactives. 🤝</p>
+          <p>Actuellement, je suis diplômé d'un BAC Général avec les spécialités STMG.</p>
+          <p>L'année prochaine, j'envisage de continuer dans une formation informatique en alternance.</p>
+        </div>
+      </div>
     </div>
-  </section>
+  </section>  
 
   <section id="formation">
     <div class="content">
-      <h2>Formation</h2>
-      <p>Détails sur mes études.</p>
+      
+      <h1>Le BTS SIO, qu'est-ce que c'est ?</h1>
+      <div class="ligne"></div>
+      <p>Le Brevet de Technicien Supérieur - Services Informatiques aux Organisations (BTS SIO) s'adresse à ceux qui souhaitent se former aux métiers d'administrateurs réseau ou de
+        développeur en deux ans. Par la suite, il est possible de continuer ses études dans le domaine de l'informatique ou d'intégrer directement le marché du travail.</p>
+      <h2>Le BTS SIO propose deux spécialités :</h2>
+      <div class="formation-grid">
+        <div class="left-column">
+          <h2>L'option SISR</h2>
+          <ul>
+            <li>SISR (Solutions d’Infrastructure, Systèmes et Réseaux) : Cette spécialité forme des administrateurs réseau qui gèrent l’installation et la maintenance des infrastructures réseaux.</li>
+          </ul>
+        </div>
+        <div class="right-column">
+          <h2>L'option SISR</h2>
+          <ul>
+            <li>SLAM (Solutions Logicielles et Applications Métiers) : Cette spécialité forme des développeurs capables de concevoir des applications informatiques et des sites web.</li>
+          </ul>
+        </div>
+      </div>
     </div>
   </section>
-
+  
   <section id="skills">
     <div class="content">
       <h2>Compétences</h2>
       <p>Liste de mes compétences.</p>
+      <div class="skills-grid">
+        <div class="skill">
+          <span>HTML</span>
+          <div class="bar">
+            <div class="progress" style="width: 50%;"></div>
+          </div>
+        </div>
+        <div class="skill">
+          <span>CSS</span>
+          <div class="bar">
+            <div class="progress" style="width: 50%;"></div>
+          </div>
+        </div>
+        <div class="skill">
+          <span>JavaScript</span>
+          <div class="bar">
+            <div class="progress" style="width: 20%;"></div>
+          </div>
+        </div>
+        <div class="skill">
+          <span>C#</span>
+          <div class="bar">
+            <div class="progress" style="width: 80%;"></div>
+          </div>
+        </div>
+        <div class="skill">
+          <span>Node.js</span>
+          <div class="bar">
+            <div class="progress" style="width: 40%;"></div>
+          </div>
+        </div>
+        <div class="skill">
+          <span>SQL</span>
+          <div class="bar">
+            <div class="progress" style="width: 30%;"></div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
-
+  
   <section id="resume">
     <div class="content">
       <h2>Parcours</h2>
       <p>Mon parcours professionnel.</p>
+      <div class="resume-list">
+        <div class="resume-item">
+          <div class="circle"></div>
+          <div class="resume-details">
+            <h3>ITIC Paris 20ème</h3>
+            <p>2ème année de BTS SIO option SLAM<br>2024-2025</p>
+          </div>
+        </div>
+        <div class="resume-item">
+          <div class="circle"></div>
+          <div class="resume-details">
+            <h3>Institut Européen F2I - Vincennes</h3>
+            <p>1ère année de BTS SIO option SLAM<br>2022 - 2023</p>
+          </div>
+        </div>
+        <div class="resume-item">
+          <div class="circle"></div>
+          <div class="resume-details">
+            <h3>Lycée Camille Claudel</h3>
+            <p>Baccalauréat STMG option Gestion Finance - Mention AB<br>2020 - 2022</p>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 
-  <section id="portfolio">
+  <section id="experience">
     <div class="content">
       <h2>Expériences</h2>
       <p>Mes expériences professionnelles.</p>
+    </div>
+  </section>
+  
+
+  <section id="projets">
+    <div class="content">
+      <h2>Mes projets personnels</h2>
+      <p>Cet article présente des projets réalisés dans le cadre de l'épreuve E4. Il inclut des sites web développés en HTML, CSS, JSON et JavaScript</p>
+      <p>Voici les liens vers les projets :</p>
+
+      <p><strong>Projet 1:</strong> Site Internet de vente en ligne.</p>
+      <a href="https://gowstpm.github.io/Shopend/"><img src="images/image2.png" class="imgProjets"></a>
+      <p><strong>Projet 2:</strong> Site Barber Shop.</p>
+      <a href="https://gowstpm.github.io/MyBarber/"><img src="images/image3.png" class="imgProjets"></a>
+
     </div>
   </section>
 
@@ -79,8 +181,30 @@
     <div class="content">
       <h2>Veille</h2>
       <p>Ma veille technologique.</p>
+      <div class="service-container">
+        <div class="service-box light-blue">
+          <h3>01. L’intégration de l'intelligence artificielle</h3>
+          <p>
+            L'intelligence artificielle (IA) est de plus en plus utilisée pour des fonctionnalités telles que la reconnaissance vocale, la détection d'anomalies et la personnalisation des scénarios. L'IA s'adapte aux préférences des occupants de manière plus proactive et intelligente.
+          </p>
+          <p>
+            <a href="https://www.phonandroid.com/des-chercheurs-creent-une-ia-qui-fait-perdre-leur-temps-aux-arnaqueurs-par-telephone.html" target="_blank">Lire la suite</a>
+          </p>
+        </div>
+        <div class="service-box dark-blue">
+          <h3>02. La sécurité renforcée</h3>
+          <p>
+            La sécurité dans la domotique est devenue une priorité absolue. Les fabricants investissent davantage dans des fonctionnalités de sécurité avancées, telles que le chiffrement des données, l'authentification à deux facteurs et les mises à jour régulières du micrologiciel, afin de protéger les utilisateurs contre les cyberattaques et les violations de la vie privée.
+          </p>
+          <p>
+            <a href="https://www.futura-sciences.com/tech/questions-reponses/cybersecurite-maisons-connectees-sont-elles-susceptibles-faire-hacker-16615/" target="_blank">Lire la suite</a>
+          </p>
+        </div>
+      </div>
     </div>
   </section>
+  
+  
 
   <section id="contact">
     <div class="content">
